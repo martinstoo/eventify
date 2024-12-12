68a812a9e8ecf164c73ad339f71fa4b1f6add2cc
martinst06 <mstoyanov@adobe.com>
2024-12-12 20:12:26 +0100

    chore: updated io.ionic.starter to com.martin.stoyanov.eventify

---
+++ android/app/src/main/java/com/martin/stoyanov/eventify/MainActivity.java
@@ -0,0 +1,5 @@
+package com.martin.stoyanov.eventify;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.martin.stoyanov.eventify',
   appName: 'Eventify',
   webDir: 'www',
   bundledWebRuntime: false,
